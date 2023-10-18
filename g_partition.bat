e7746915d9593aadbcfbfd5be72b6d22c57aa072
F.Zabihian <140643700+faramarz-zabihian@users.noreply.github.com>
2023-10-18 17:52:09 +0330

    Update g_partition.bat

--- g_partition.bat
+++ g_partition.bat
@@ -1,4 +1,4 @@
 @echo usage g_partition partitions
 @echo rem https://docs.espressif.com/projects/esp8266-rtos-sdk/en/latest/api-guides/partition-tables.html
 @echo --------------------------------
-@ python D:\AI\DEV_ESP8266\msys32\home\asus\ESP8266_RTOS_SDK\components\partition_table\gen_esp32part.py --verify %1.csv %1.bin
+@ python D:\DEV_ESP8266\msys32\home\asus\ESP8266_RTOS_SDK\components\partition_table\gen_esp32part.py --verify %1.csv %1.bin
